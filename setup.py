8af5a47558a737b2c87b105b1f5f579cca4e2b5e
Timotheus Kampik <timotheus.kampik@signavio.com>
2016-07-30 23:03:56 +0200

    initial commit

---
+++ setup.py
@@ -0,0 +1,25 @@
+import codecs, setuptools
+
+
+setuptools.setup(
+    name='sphinxprettysearchresults',
+    version='0.1',
+    description='Decently styled search results for sphinx-doc projects',
+    author='Timotheus Kampik',
+    author_email='timotheus.kampik@gmail.com',
+    url='https://github.com/TimKam/sphinx-pretty-searchresults',
+    platforms=["any"],
+    license="BSD",
+    zip_safe=False,
+    install_requires=[],
+    classifiers=[
+        "Development Status :: 4 - Beta",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python",
+        "Environment :: Plugins",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: MIT License",
+        "Topic :: Documentation",
+    ],
+    long_description=codecs.open("README.rst", "r", "utf-8").read(),
+)
---
+++ sphinxprettysearchresults/__init__.py
@@ -0,0 +1,53 @@
+import os, re, shutil, subprocess, sys
+from shutil import move
+
+
+def remove_markup():
+    # remove remaining markup from .txt files that are used to display search results
+
+    regexp = re.compile("\*\**|"
+           "\=\=*|"
+           "\~\~*|"
+           "\^\^*|"
+           "\-\-*|"
+           "(\[image\])*")
+
+    for subdir, dirs, files in os.walk('../_build_txt'):
+        for file in files:
+            if file.endswith('.txt'):
+                path = os.path.join(subdir, file)
+                with open(path) as infile:
+                    with open(file,'w') as new_file:
+                        for line in infile:
+                            line = re.sub(regexp, '', line)
+                            new_file.write(line)
+                os.remove(path)
+                move(file, path)
+
+
+def clean_txts(path, language, outdir):
+    sys.path.append(path)
+
+    sources_path =  outdir + '/_sources'
+    sources_build_path = '../_build_txt'
+    if os.path.isdir(sources_path):
+        shutil.rmtree(sources_path)
+
+    if not os.path.isdir(sources_build_path):
+        os.makedirs(sources_build_path)
+
+    build_txt = subprocess.Popen(['sphinx-build', '-a', '-b', 'text','-D' 'language=' + language,\
+                                   '.', sources_build_path])
+    build_txt.wait()
+    remove_markup()
+    shutil.move(sources_build_path, sources_path)
+
+
+def build_search_snippets(app, docname):
+    if app.builder.name == 'html':
+        project_path = os.path.dirname(os.path.realpath('%s/..' % __file__))
+        clean_txts(project_path, app.config.language, app.outdir)
+
+
+def setup(app):
+    app.connect('build-finished', build_search_snippets)

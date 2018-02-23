80104de977f6f5d62ebca8fb4fc3bfd43a082438
Timotheus Kampik <timotheus.kampik@gmail.com>
2018-02-23 22:53:26 +0100

    version bump

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ import codecs, setuptools
 setuptools.setup(
     name='sphinxprettysearchresults',
     packages=['sphinxprettysearchresults'],
-    version='0.3.2',
+    version='0.3.3',
     description='Decently styled search results for sphinx-doc projects',
     author='Timotheus Kampik',
     author_email='timotheus.kampik@gmail.com',
